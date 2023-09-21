d3386b6b8ec82e27da270b0e42e366172ac01142
daring <danila.daring@gmail.com>
2023-09-21 13:54:55 +0300

    init project

---
+++ src/main/java/ru/itsyn/sandbox/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package ru.itsyn.sandbox.view.user;
+
+import ru.itsyn.sandbox.entity.User;
+import ru.itsyn.sandbox.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("sb_User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
