cb99f055d9156a30c146f265162272ba46f25c95
daring <danila.daring@gmail.com>
2023-09-21 14:49:17 +0300

    SandboxView - add userField and removeActions button

--- src/main/java/ru/itsyn/sandbox/view/sandbox/SandboxView.java
+++ src/main/java/ru/itsyn/sandbox/view/sandbox/SandboxView.java
@@ -1,15 +1,26 @@
 package ru.itsyn.sandbox.view.sandbox;
 
 
+import com.vaadin.flow.component.ClickEvent;
+import io.jmix.flowui.component.combobox.EntityComboBox;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.view.*;
+import ru.itsyn.sandbox.entity.User;
 import ru.itsyn.sandbox.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.StandardView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
 
 @Route(value = "SandboxView", layout = MainView.class)
 @ViewController("sb_SandboxView")
 @ViewDescriptor("sandbox-view.xml")
 public class SandboxView extends StandardView {
+
+    @ViewComponent
+    private EntityComboBox<User> userField;
+
+    @Subscribe("removeActions")
+    public void onRemoveActionsClick(final ClickEvent<JmixButton> event) {
+        userField.removeAllActions();
+    }
+
 }
