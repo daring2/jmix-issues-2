ba397d8ed8456be9cc5d8f3b1e6f61ffeb9190a5
daring <danila.daring@gmail.com>
2023-09-21 15:46:14 +0300

    SandboxView - add testAction

--- src/main/java/ru/itsyn/sandbox/view/sandbox/SandboxView.java
+++ src/main/java/ru/itsyn/sandbox/view/sandbox/SandboxView.java
@@ -3,6 +3,7 @@ package ru.itsyn.sandbox.view.sandbox;
 
 import com.vaadin.flow.component.ClickEvent;
 import io.jmix.flowui.component.combobox.EntityComboBox;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
 import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
 import ru.itsyn.sandbox.entity.User;
@@ -21,6 +22,12 @@ public class SandboxView extends StandardView {
     @Subscribe("removeActions")
     public void onRemoveActionsClick(final ClickEvent<JmixButton> event) {
         userField.removeAllActions();
+    }
+
+    @Subscribe("testAction")
+    public void onTestActionPerformed(final ActionPerformedEvent event) {
+        System.out.println("SandboxView.onTestActionPerformed");
+//        new Exception().printStackTrace();
     }
 
 }
